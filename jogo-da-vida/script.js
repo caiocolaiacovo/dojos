426ec7846f02ef590ec7d4a87e85167dcfc46a68
Caio Colaiacovo <caiohshot@gmail.com>
2017-11-08 21:59:44 -0300

    Criando grid

--- jogo-da-vida/script.js
+++ jogo-da-vida/script.js
@@ -1,1 +1,15 @@
-console.log('teste2');
+document.addEventListener("DOMContentLoaded", function(event) { 
+    var table = document.getElementById('table');
+    
+    for (var i = 0; i < 10; i++) {
+        var novaLinha = table.insertRow(i);
+
+        for (var j = 0; j < 10; j++) {
+            var novaCelula = novaLinha.insertCell(j);
+
+            novaCelula.innerHTML = '(' + i + ',' + j + ')';
+        }
+    }
+
+    console.log('table.row[2]: ', table.rows[2].cells[0]);
+});
