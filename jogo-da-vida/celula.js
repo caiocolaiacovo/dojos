a92f3fed57cd174ead3efdac569e684bfb4b6d62
Marlom Souza <thunder_bolt2000@hotmail.com>
2017-11-11 21:03:40 -0300

    exibir geracao

--- jogo-da-vida/celula.js
+++ jogo-da-vida/celula.js
@@ -1,8 +1,38 @@
 function Celula(linha, coluna) {
     this.linha = linha;
     this.coluna = coluna;
+    this.vida = true;
 
     Celula.prototype.posicaoCima = function() {
+        return linha - 1;
+    };
+
+    Celula.prototype.posicaoCimaEsquerda = function() {
+        return this.linha - 1;
+    };
+
+    Celula.prototype.posicaoBaixo = function() {
         return this.linha + 1;
     };
+
+    Celula.prototype.posicaoEsquerda = function() {
+        return this.linha - 1;
+    };
+
+    Celula.prototype.posicaoDireita = function() {
+        return this.linha + 1;
+    };
+
+    Celula.prototype.celulaEmVolta = function() {
+        return this.linha + 1;
+    };
+
+    Celula.prototype.MostrarVida = function(valor){
+        console.log("valor",valor)
+        if(this.vida)
+            valor.style.backgroundColor = "green";
+    };
+
+
+
 }
--- jogo-da-vida/script.js
+++ jogo-da-vida/script.js
@@ -1,6 +1,21 @@
+var numeroGeracao = 0;
+var labelGeracao = {};
+
 document.addEventListener("DOMContentLoaded", function(event) { 
-    var table = document.getElementById('table');
-    
+    var $ = document.getElementById.bind(document);
+    var table = $('table');
+    labelGeracao = $('numeroGeracao');
+
+    CriarGrid(table);
+
+    var celula = new Celula(1, 1);
+
+    celula.MostrarVida(table.rows[2].cells[0]);
+    console.log(celula.posicaoCima());
+    exibirGeracao()
+});
+
+function CriarGrid(table){
     for (var i = 0; i < 10; i++) {
         var novaLinha = table.insertRow(i);
 
@@ -8,9 +23,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
             var novaCelula = novaLinha.insertCell(j);
         }
     }
+}
 
-    console.log('table.row[2]: ', table.rows[2].cells[0]);
+function exibirGeracao(){
 
-    var celula = new Celula(1, 1);
-    console.log(celula.posicaoCima());
-});
+    labelGeracao.innerHTML = "Geracao: " + (++numeroGeracao) ;
+    setTimeout(function(){ exibirGeracao()}, 1000);
+}
